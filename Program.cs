e190424fb41065d0d84f7a3f4bee46a0750d6e54
michael innamorato <michaelinnamorato94@gmail.com>
2020-11-20 15:37:58 -0500

    Add files via upload
    program3

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge;
+            userAge = 25;
+            userAge = userAge + 2;
+            Console.WriteLine($"Hello World!, you are {userAge} years old");
+        }
+    }
+}
