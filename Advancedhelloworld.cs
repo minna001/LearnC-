6cb3ce3a251f3df6518ae04aad5352c1d9124bbd
Innamorato <minnamorato@dep.nyc.gov>
2020-11-20 14:44:32 -0500

    first commit

---
+++ Advancedhelloworld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LearnC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            var day = DateTime.Today;
+            Console.WriteLine($"\nHello {name}, on {day:d}, {date:t}!");
+            Console.Write("\nPress any key to exit ...");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LearnC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
